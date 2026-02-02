30f1e0140a59c61c31da048b4fd711df0b7ec16a
Steven Huang <22431150+EvenStevenH@users.noreply.github.com>
2026-02-02 14:50:14 -0500

    Set up local storage for theme

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-document.getElementById("toggleDarkModeButton").addEventListener("click", toggleDarkMode)
+// light/dark mode
+const themeBtn = document.getElementById("themeBtn");
+const root = document.documentElement;
+const isDark = root.classList.contains("dark-mode");
 
-function toggleDarkMode() {
-   let element = document.body;
-   element.classList.toggle("dark-mode");
-}
+themeBtn.setAttribute("aria-checked", isDark);
+themeBtn.textContent = isDark ? "Light Mode" : "Dark Mode";
+
+themeBtn.addEventListener("click", () => {
+	const nowDark = root.classList.toggle("dark-mode");
+
+	themeBtn.setAttribute("aria-checked", nowDark);
+	themeBtn.textContent = nowDark ? "Light Mode" : "Dark Mode";
+	localStorage.setItem("theme", nowDark ? "dark" : "light");
+});
+
+// main functions
+const searchInput = document.getElementById("search-input");
+const filterInput = document.getElementById("filter-input");
+const countryList = document.getElementById("country-list");
+
+let countryArray = [];
