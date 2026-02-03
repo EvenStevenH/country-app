58627bcb0565926cd7bc7af8310775973985691c
Steven Huang <22431150+EvenStevenH@users.noreply.github.com>
2026-02-03 17:19:07 -0500

    Add function to display country info

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-// light/dark mode
+// get elements and set up list
 const themeBtn = document.getElementById("themeBtn");
 const root = document.documentElement;
 const isDark = root.classList.contains("dark-mode");
+const searchInput = document.getElementById("search-input");
+const filterInput = document.getElementById("filter-input");
+const countryList = document.getElementById("country-list");
+const countryInfo = document.getElementById("country-info");
 
+let allCountries = [];
+
+// light/dark mode
 themeBtn.setAttribute("aria-checked", isDark);
 themeBtn.textContent = isDark ? "Light Mode" : "Dark Mode";
 themeBtn.addEventListener("click", () => {
@@ -13,24 +20,18 @@ themeBtn.addEventListener("click", () => {
 	localStorage.setItem("theme", nowDark ? "dark" : "light");
 });
 
-// main functions
-const searchInput = document.getElementById("search-input");
-const filterInput = document.getElementById("filter-input");
-const countryList = document.getElementById("country-list");
-let allCountries = [];
+// update list if any input is given
+searchInput.addEventListener("input", updateFullResults);
+filterInput.addEventListener("change", updateFullResults);
 
 function filterResults(countryData, region) {
-	if (!region) return countryData;
+	if (!region) return countryData; // shows all countries if no region is selected
 	return countryData.filter((country) => country.region.toLowerCase().includes(region.toLowerCase().trim()));
 }
 
 function searchResults(countryData, searchQuery) {
 	return countryData.filter((country) => country.name.common.toLowerCase().includes(searchQuery.toLowerCase().trim()));
 }
-
-// update list if any input is given
-searchInput.addEventListener("input", updateFullResults);
-filterInput.addEventListener("change", updateFullResults);
 
 function updateFullResults() {
 	const searchTerm = searchInput.value.toLowerCase().trim();
@@ -45,31 +46,75 @@ function updateFullResults() {
 
 // API fetch
 async function fetchCountries() {
-	const response = await fetch("https://restcountries.com/v3.1/all?fields=name,population,region,capital,flags");
+	const response = await fetch("https://restcountries.com/v3.1/all?fields=name,population,region,capital,flags,subregion,languages,tld,currencies,borders");
 	const data = await response.json();
 
 	allCountries = data;
 	displayCountries(data);
 }
 
-function displayCountries(countriesData) {
+// home page
+function displayCountries(countryData) {
 	countryList.innerHTML = "";
 
-	countriesData.forEach((country) => {
+	countryData.forEach((country) => {
 		const li = document.createElement("li");
 		li.classList.add("country-item");
 
+		// li.innerHTML = `
+		// <a href="countryinfo.html">
+		// 	<img class="country-flag" src="${country.flags.png}">
+		// 	<div class="country-info">
+		// 		<h2>${country.name.common}</h2>
+		// 		<p><strong>Population</strong>: ${country.population}<p>
+		// 		<p><strong>Region</strong>: ${country.region}<p>
+		// 		<p><strong>Capital</strong>: ${country.capital?.[0] ?? "N/A"}<p>
+		// 	</div>
+		// </a>`;
 		li.innerHTML = `
-        <img class="country-flag" src="${country.flags.png}">
-        <div class="country-info">
-            <h2>${country.name.common}</h2>
-			<p><strong>Population</strong>: ${country.population}<p>
-			<p><strong>Region</strong>: ${country.region}<p>
-			<p><strong>Capital</strong>: ${country.capital?.[0] ?? "N/A"}<p>
-        </div>`;
+			<img class="country-flag" src="${country.flags.png}">
+			<div class="country-info">
+				<h2>${country.name.common}</h2>
+				<p><strong>Population</strong>: ${country.population}</p>
+				<p><strong>Region</strong>: ${country.region}</p>
+				<p><strong>Capital</strong>: ${country.capital?.[0] ?? "N/A"}</p>
+			</div>`;
+
+		li.addEventListener("click", () => {
+			displayCountryInfo(country);
+		});
 
 		countryList.appendChild(li);
 	});
+}
+
+// country info page
+function displayCountryInfo(country) {
+	countryInfo.innerHTML = "";
+
+	countryInfo.innerHTML = `
+        <img class="country-flag" src="${country.flags.png}">
+
+        <div class="country-info">
+            <h2>${country.name.common}</h2>
+			
+			<ul>
+				<li><strong>Native Name</strong>: ${Object.values(country.name.nativeName ?? {})[0]?.common ?? "N/A"}</li>
+				<li><strong>Population</strong>: ${country.population}</li>
+				<li><strong>Region</strong>: ${country.region}</li>
+				<li><strong>Sub Region</strong>: ${country.subregion}</li>
+				<li><strong>Capital</strong>: ${country.capital?.[0] ?? "N/A"}</li>
+				<li><strong>Top Level Domain</strong>: ${country.tld?.[0] ?? "N/A"}</li>
+				<li><strong>Currencies</strong>: ${Object.values(country.currencies ?? {})
+					.map((c) => c.name)
+					.join(", ")}</li>
+				<li><strong>Languages</strong>: ${Object.values(country.languages ?? {}).join(", ")}</li>
+			</ul>
+
+        	<div class="border-countries">
+				<strong>Border Countries</strong>: ${country.borders?.join(", ") ?? "None"}
+			</div>
+        </div>`;
 }
 
 fetchCountries();
